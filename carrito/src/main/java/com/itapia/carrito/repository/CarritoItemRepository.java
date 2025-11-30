b865790ad570567d25a3fa50e591b935e3a2c76e
Lajerarquia <ben.palmag@duocuc.cl>
2025-11-30 12:32:56 -0300

    Delete carrito directory

--- carrito/src/main/java/com/itapia/carrito/repository/CarritoItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itapia.carrito.repository;
-
-import com.itapia.carrito.entity.CarritoItemEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CarritoItemRepository extends JpaRepository<CarritoItemEntity, Long> {
-    List<CarritoItemEntity> findByCarritoId(Long carritoId);
-}
--- carrito/src/main/java/com/itapia/carrito/repository/CarritoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itapia.carrito.repository;
-
-import com.itapia.carrito.entity.CarritoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface CarritoRepository extends JpaRepository<CarritoEntity, Long> {
-    Optional<CarritoEntity> findByUsuario(String usuario);
-}
