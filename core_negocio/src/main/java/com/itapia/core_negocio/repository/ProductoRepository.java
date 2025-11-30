0198867304609144e0a9bbeaf72d1d9ea50c39ea
Lajerarquia <ben.palmag@duocuc.cl>
2025-11-30 12:33:05 -0300

    Delete core_negocio directory

--- core_negocio/src/main/java/com/itapia/core_negocio/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itapia.core_negocio.repository;
-
-import com.itapia.core_negocio.entity.ProductoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ProductoRepository extends JpaRepository<ProductoEntity, Long> {
-    List<ProductoEntity> findByCategoriaId(Long categoriaId);
-}
