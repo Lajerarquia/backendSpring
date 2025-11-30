8973e0f57f88df7252342e4ce92b2b5062f691b8
Lajerarquia <ben.palmag@duocuc.cl>
2025-11-30 12:33:12 -0300

    Delete gestion-usuarios directory

--- gestion-usuarios/src/main/java/com/itapia/gestion_usuarios/repository/RolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itapia.gestion_usuarios.repository;
-
-import com.itapia.gestion_usuarios.entity.RolEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface RolRepository extends JpaRepository<RolEntity, Long> {
-    Optional<RolEntity> findByNombre(String nombre);
-}
